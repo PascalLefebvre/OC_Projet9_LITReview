7d0fef450a016c5dedb6f5b8139079d72192f8e2
Pascal Lefebvre <skrova2017@gmail.com>
2022-12-19 16:28:03 +0100

    End of the posts page layout.

--- bookreview/templatetags/bookreview_extras.py
+++ bookreview/templatetags/bookreview_extras.py
@@ -3,6 +3,9 @@
 from django import template
 
 from bookreview.models import Review
+from django.utils.safestring import mark_safe
+
+MAX_RATING = 5
 
 register = template.Library()
 
@@ -14,9 +17,19 @@ def already_reviewed(ticket, user):
     else:
         return False
 
+@register.filter
+def display_stars_rating(rating):
+    """ Change the rating display from integer to stars. """
+    string = ''
+    for i in range(rating):
+        string += '  ' + '<span class="bi bi-star-fill fw-bold"></span>'
+    for i in range(rating, MAX_RATING):
+        string += '  ' + '<span class="bi bi-star fw-bold"></span>'
+    return mark_safe(string)
+
 @register.simple_tag(takes_context=True)
-def get_poster_display(context, user):
-    """ Check if the logged user is the author of the post (ticket or review). """
+def get_post_author(context, user):
+    """ Return the author of the post (ticket or review). """
     if user == context['user']:
         return 'Vous'
     return f"{user.username}"
