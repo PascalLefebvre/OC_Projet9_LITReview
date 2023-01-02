7e2b2382f2eff2e592cf7fb5dd372a302dae706c
Pascal Lefebvre <skrova2017@gmail.com>
2023-01-02 08:52:23 +0100

    Revert "The path of the 'bookreview' URL pattern changed from 'bookreview' to 'bookreview/'."
    This reverts commit 89ca483c5f768669bbba232623fee2a4148a500c.

--- litreview/urls.py
+++ litreview/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("accounts.urls")),
-    path("bookreview/", include("bookreview.urls")),
+    path("bookreview", include("bookreview.urls")),
 ]
 
 if settings.DEBUG:
